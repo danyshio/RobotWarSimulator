2de0c8a4b03d2dfd33499c35117a17a89d9e3a6f
danyshio <muqhreezdanysh@gmail.com>
2025-05-22 22:41:49 +0800

    battlefield layout

--- main.cpp
+++ main.cpp
@@ -10,7 +10,60 @@ Course: Data Structures and Algorithms
  Phone: 010-5324386
  **********|**********|**********/
 
- #include <iostream>
- #include<string>
+#include <iostream>
+#include <fstream>
+#include <sstream>
+#include <vector>
+#include <string>
+using namespace std;
 
- using namespace std;
+class Battlefield {
+private:
+    int rows;
+    int cols;
+    vector<vector<char>> grid;
+
+public:
+    Battlefield(int r, int c) : rows(r), cols(c) {
+        grid.resize(rows, vector<char>(cols, '.'));
+    }
+
+    void print() const {
+        for (int i = 0; i < rows; ++i) {
+            for (int j = 0; j < cols; ++j) {
+                cout << grid[i][j] << ' ';
+            }
+            cout << endl;
+        }
+    }
+};
+
+int main() {
+    ifstream inFile("input.txt");
+    if (!inFile) {
+        cerr << "Failed to open input.txt" << endl;
+        return 1;
+    }
+
+    string line;
+    int rows = 0, cols = 0, steps = 0, robotCount = 0;
+
+    // Read "M by N : rows cols"
+    getline(inFile, line);
+    istringstream dimStream(line.substr(line.find(':') + 1));
+    dimStream >> rows >> cols;
+
+    // Read "steps: xxx"
+    getline(inFile, line);
+    steps = stoi(line.substr(line.find(':') + 1));
+
+    // Read "robots: xxx"
+    getline(inFile, line);
+    robotCount = stoi(line.substr(line.find(':') + 1));
+
+    // Print the battlefield
+    Battlefield field(rows, cols);
+    field.print();
+
+    return 0;
+}
