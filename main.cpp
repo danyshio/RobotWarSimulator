d3e00464dadef507b15f9bf5c263a349f4ca9ef5
danyshio <muqhreezdanysh@gmail.com>
2025-05-22 11:11:39 +0800

    First commit
    test

---
+++ main.cpp
@@ -0,0 +1,16 @@
+ /**********|**********|**********|
+ Program: YOUR_FILENAME.cpp / YOUR_FILENAME.h 
+Course: Data Structures and Algorithms
+ Trimester: 2410
+ Name: Frank Carrano
+ ID: 1071001234
+ Lecture Section: TC3L
+ Tutorial Section: T11L
+ Email: abc123@yourmail.com
+ Phone: 010-5324386
+ **********|**********|**********/
+
+ #include <iostream>
+ #include<string>
+
+ using namespace std;
