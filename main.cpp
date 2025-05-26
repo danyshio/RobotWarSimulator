4bc79c32d07c5b9a7e1037315f8e7144b322c020
danyshio <muqhreezdanysh@gmail.com>
2025-05-26 21:17:41 +0800

    updated battlefield layout

--- main.cpp
+++ main.cpp
@@ -15,6 +15,8 @@ Course: Data Structures and Algorithms
 #include <sstream>
 #include <vector>
 #include <string>
+#include <cstdlib>
+#include <ctime>
 using namespace std;
 
 class Battlefield {
@@ -28,6 +30,14 @@ public:
         grid.resize(rows, vector<char>(cols, '.'));
     }
 
+    bool placeRobot(int r, int c, char symbol) {
+        if (r >= 0 && r < rows && c >= 0 && c < cols && grid[r][c] == '.') {
+            grid[r][c] = symbol;
+            return true;
+        }
+        return false;
+    }
+
     void print() const {
         for (int i = 0; i < rows; ++i) {
             for (int j = 0; j < cols; ++j) {
@@ -39,6 +49,8 @@ public:
 };
 
 int main() {
+    srand(time(0)); // seed for randomness
+
     ifstream inFile("input.txt");
     if (!inFile) {
         cerr << "Failed to open input.txt" << endl;
@@ -61,8 +73,47 @@ int main() {
     getline(inFile, line);
     robotCount = stoi(line.substr(line.find(':') + 1));
 
-    // Print the battlefield
+    // Create battlefield
     Battlefield field(rows, cols);
+
+    // Read robot data
+    for (int i = 0; i < robotCount; ++i) {
+        getline(inFile, line);
+        istringstream robotStream(line);
+        string type, name, rowStr, colStr;
+        robotStream >> type >> name >> rowStr >> colStr;
+
+        int r, c;
+
+        if (rowStr == "random")
+            r = rand() % rows;
+        else
+            r = stoi(rowStr);
+
+        if (colStr == "random")
+            c = rand() % cols;
+        else
+            c = stoi(colStr);
+
+        // Use first letter of robot name as symbol (uppercase)
+        char symbol = toupper(name[0]);
+
+        // Try placing robot, retry random if space is taken
+        int attempts = 0;
+        while (!field.placeRobot(r, c, symbol) && attempts < 100) {
+            r = rand() % rows;
+            c = rand() % cols;
+            ++attempts;
+        }
+    }
+
+    // ignore first, this is just for reference
+    cout << "----- Battlefield Layout from input.txt -----" << endl;
+    cout << "Rows: " << rows << ", Columns: " << cols << endl;
+    cout << "Steps: " << steps << endl;
+    cout << "Robots: " << robotCount << endl << endl;
+
+    cout << "----- Battlefield Layout -----" << endl;
     field.print();
 
     return 0;
